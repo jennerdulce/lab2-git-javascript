9a0faaaee386d1ae1cf37751584fe667783e47be
jennerdulce <jennerdulce@gmail.com>
2025-09-14 11:23:07 -0700

    feat: added functionality to capitalize function in strings.js

--- strings.js
+++ strings.js
@@ -22,7 +22,11 @@ export function reverse(str) {
  * @returns {string} Capitalized string
  */
 export function capitalize(str) {
-  return "stubbed";
+  if (typeof str !== 'string') {
+    throw new Error('Input must be a string');
+  }
+  if (str.length === 0) return '';
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
 
 /**
