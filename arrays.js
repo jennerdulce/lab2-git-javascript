c7a636bcf7fe5a3f5fa47781524e56e39996fc19
jennerdulce <jennerdulce@gmail.com>
2025-09-14 12:55:01 -0700

    feat: added average and filterEven functions to arrays.js

--- arrays.js
+++ arrays.js
@@ -14,3 +14,27 @@ export function sum(arr) {
   }
   return arr.reduce((acc, num) => acc + num, 0)
 }
+
+/**
+ * Calculate average of array
+ * @param {number[]} arr - Array of numbers
+ * @returns {Number} Average value
+ */
+export function average(arr) {
+  if(!Array.isArray(arr) || arr.length === 0) {
+    throw new Error('Input must be a non-empty array');
+  }
+  return sum(arr) / arr.length;
+}
+
+/**
+ * Filter even numbers
+ * @param {number[]} arr - Array of numbers
+ * @returns {number[]} Array of even numbers
+ */
+export function filterEven(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Input must be an array');
+  }
+  return arr.filter(num => num % 2 === 0);
+}
