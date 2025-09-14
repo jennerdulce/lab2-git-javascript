592fabc528a135c42c7f9edf13a92a7b1de8de1e
jennerdulce <jennerdulce@gmail.com>
2025-09-14 11:15:10 -0700

    feature: Scaffold strings module and add console tests to main file

--- index.js
+++ index.js
@@ -5,9 +5,10 @@
  */
 
 // Import math function
-import * as math from './math.js'
+import * as math from './math.js';
+import * as strings from './strings.js';
 
-console.log('Testing Math Functions');
+console.log('\n=== Testing Math Module ===');
 console.log('add(5, 3) = ', math.add(5, 3)); // Should be 8
 console.log('subtract(10, 4) = ', math.subtract(10, 4)); // Should be 6
 console.log('mulitply(3, 7) = ', math.multiply(3, 7)); // Should be 21
@@ -15,3 +16,10 @@ console.log('divide(20, 4) = ', math.divide(20, 4)); // Should be 5
 console.log('power(2, 8) = ', math.power(2, 8)); // Should be 256
 console.log('sqrt(16) = ', math.sqrt(16)); // Should be 4
 console.log('factorial(5) = ', math.factorial(5)); // Should be 120
+
+console.log('\n=== Testing String Module ===');
+console.log('reverse("Hello") = ', strings.reverse('hello'));
+console.log('capitalize("javascript") = ', strings.capitalize('javascript'));
+console.log('countVowels("hello world") = ', strings.countVowels('hello world'));
+console.log('isPalindrome("racecar") = ', strings.isPalindrome('racecar'));
+console.log('isPalindrome("hello") = ', strings.isPalindrome('hello'));
---
+++ strings.js
@@ -0,0 +1,41 @@
+/**
+ * String utilities for learning purposes
+ * @file 'strings.js'
+ * @author 'Jenner D. Dulce
+ */
+
+/**
+ * Reverse a string
+ * @param {string} str - String to reverse
+ * @returns {string} Reversed String
+ */
+export function reverse(str) {
+  return "stubbed";
+}
+
+/**
+ * Capitalize the first letter of a string
+ * @param {string} str - String to capitalize
+ * @returns {string} Capitalized string
+ */
+export function capitalize(str) {
+  return "stubbed";
+}
+
+/**
+ * Count vowels in a string
+ * @param {string} str - String to count vowels
+ * @returns {number} Number of vowels
+ */
+export function countVowels(str) {
+  return "stubbed";
+}
+
+/**
+ * Check if a string is a palindrome
+ * @param {string} - String to check
+ * @returns {boolean} True if palindrome
+ */
+export function isPalindrome(str) {
+  return "stubbed";
+}
